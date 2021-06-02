0ac241485254391f343da04d45907e32b29ab60a
ChristopherBiscardi <chris@christopherbiscardi.com>
2021-06-02 11:38:17 -0700

    output format

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use owo_colors::FgDynColorDisplay;
 use owo_colors::{CssColors, OwoColorize};
 use serde::Deserialize;
 use std::collections::HashMap;
@@ -13,19 +14,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match opt.command {
         Opt::Search { search_term } => {
-            let resp = client
+            let response = client
                 .get("https://api.waqi.info/search/")
                 .query(&[("token", &opt.api_token), ("keyword", &search_term)])
                 .send()
-                .await?
-                .json::<WeatherResponse<Vec<StationMeta>>>()
                 .await?;
 
-            for station_meta in resp.data.iter() {
-                println!(
-                    "id: {}, url: {}",
-                    station_meta.uid, station_meta.station.url
-                )
+            match opt.output {
+                OutputFormat::Human => {
+                    let resp = response.json::<WeatherResponse<Vec<StationMeta>>>().await?;
+                    for station_meta in resp.data.iter() {
+                        println!("{}", station_meta)
+                    }
+                }
+                OutputFormat::Json => {
+                    let resp = response
+                        .json::<WeatherResponse<serde_json::Value>>()
+                        .await?;
+                    println!("{}", resp.data);
+                }
+                OutputFormat::Url => {
+                    let resp = response.json::<WeatherResponse<Vec<StationMeta>>>().await?;
+                    for station_meta in resp.data.iter() {
+                        println!("{}", station_meta.station.url)
+                    }
+                }
+                OutputFormat::Debug => {
+                    let resp = response.json::<WeatherResponse<Vec<StationMeta>>>().await?;
+                    dbg!("{:?}", resp);
+                }
             }
 
             Ok(())
@@ -34,41 +51,53 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             scale,
             station_name,
         } => {
-            let resp = client
+            let response = client
                 .get(format!("https://api.waqi.info/feed/{}/", station_name))
                 .query(&[("token", opt.api_token)])
                 .send()
-                .await?
-                .json::<WeatherResponse<WeatherData>>()
                 .await?;
-            println!(
-                "{}
-aqi: {}",
-                resp.data.city.name.blue().bold(),
-                match resp.data.aqi {
-                    0..=50 => {
-                        resp.data.aqi.color(CssColors::Green)
-                    }
-                    51..=100 => {
-                        resp.data.aqi.color(CssColors::Yellow)
-                    }
-                    101..=150 => {
-                        resp.data.aqi.color(CssColors::Orange)
-                    }
-                    151..=200 => {
-                        resp.data.aqi.color(CssColors::Red)
-                    }
-                    201..=300 => {
-                        resp.data.aqi.color(CssColors::Purple)
-                    }
-                    301..=500 => {
-                        resp.data.aqi.color(CssColors::Maroon)
-                    }
-                    _ => {
-                        resp.data.aqi.color(CssColors::White)
-                    }
+            match opt.output {
+                OutputFormat::Human => {
+                    let resp = response.json::<WeatherResponse<WeatherData>>().await?;
+                    println!(
+                        "{}
+    aqi: {}",
+                        resp.data.city.name.blue().bold(),
+                        match resp.data.aqi {
+                            0..=50 => {
+                                resp.data.aqi.color(CssColors::Green)
+                            }
+                            51..=100 => {
+                                resp.data.aqi.color(CssColors::Yellow)
+                            }
+                            101..=150 => {
+                                resp.data.aqi.color(CssColors::Orange)
+                            }
+                            151..=200 => {
+                                resp.data.aqi.color(CssColors::Red)
+                            }
+                            201..=300 => {
+                                resp.data.aqi.color(CssColors::Purple)
+                            }
+                            301..=500 => {
+                                resp.data.aqi.color(CssColors::Maroon)
+                            }
+                            _ => {
+                                resp.data.aqi.color(CssColors::White)
+                            }
+                        }
+                    );
                 }
-            );
+                OutputFormat::Json => {
+                    let resp = response
+                        .json::<WeatherResponse<serde_json::Value>>()
+                        .await?;
+                    println!("{}", resp.data);
+                }
+                OutputFormat::Url => {}
+                OutputFormat::Debug => {}
+            }
+
             Ok(())
         }
     }
@@ -103,11 +132,45 @@ impl FromStr for Scale {
     }
 }
 
+#[derive(Debug)]
+enum OutputFormat {
+    Human,
+    Json,
+    Url,
+    Debug,
+}
+#[derive(Debug)]
+struct InvalidOutputFormat;
+impl fmt::Display for InvalidOutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "invalid output format")
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = InvalidOutputFormat;
+    fn from_str(temp: &str) -> Result<Self, Self::Err> {
+        match temp {
+            "h" => Ok(OutputFormat::Human),
+            "human" => Ok(OutputFormat::Human),
+            "json" => Ok(OutputFormat::Json),
+            "url" => Ok(OutputFormat::Url),
+            "d" => Ok(OutputFormat::Debug),
+            "debug" => Ok(OutputFormat::Debug),
+            _ => Err(InvalidOutputFormat),
+        }
+    }
+}
+
 #[derive(Debug, StructOpt)]
 #[structopt(name = "aqi", about = "Find weather quality from stations near you")]
 struct AQI {
     #[structopt(short = "t", long = "token", env = "API_TOKEN")]
     api_token: String,
+
+    #[structopt(short = "o", long = "output", default_value = "human")]
+    output: OutputFormat,
+
     #[structopt(subcommand)]
     command: Opt,
 }
@@ -117,12 +180,7 @@ enum Opt {
         search_term: String,
     },
     Info {
-        #[structopt(
-            short = "s",
-            long = "scale",
-            default_value = "fahrenheit",
-            parse(try_from_str)
-        )]
+        #[structopt(short = "s", long = "scale", default_value = "fahrenheit")]
         scale: Scale,
         station_name: String,
     },
@@ -143,6 +201,31 @@ struct WeatherData {
     iaqi: HashMap<String, HashMap<String, f32>>,
     forecast: Forecast,
     debug: SyncDebug,
+}
+
+impl fmt::Display for WeatherData {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let aqi = format!(
+            "{}",
+            match 0 {
+                0..=50 => self.aqi.color(CssColors::Green),
+                51..=100 => self.aqi.color(CssColors::Yellow),
+                101..=150 => self.aqi.color(CssColors::Orange),
+                151..=200 => self.aqi.color(CssColors::Red),
+                201..=300 => self.aqi.color(CssColors::Purple),
+                301..=500 => self.aqi.color(CssColors::Maroon),
+                _ => self.aqi.color(CssColors::White),
+            }
+        );
+
+        write!(
+            f,
+            "{}\n{}\naqi: {}",
+            self.city.name.blue().bold(),
+            self.city.url,
+            aqi
+        )
+    }
 }
 #[derive(Debug, Deserialize)]
 
@@ -196,9 +279,60 @@ struct StationMeta {
     uid: u32,
 }
 
+impl fmt::Display for StationMeta {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let aqi = match self.aqi.parse::<u32>() {
+            Ok(num) => format!(
+                "{}",
+                match 0 {
+                    0..=50 => num.color(CssColors::Green),
+                    51..=100 => num.color(CssColors::Yellow),
+                    101..=150 => num.color(CssColors::Orange),
+                    151..=200 => num.color(CssColors::Red),
+                    201..=300 => num.color(CssColors::Purple),
+                    301..=500 => num.color(CssColors::Maroon),
+                    _ => num.color(CssColors::White),
+                }
+            ),
+            Err(_) => format!("{}", self.aqi.white()),
+        };
+        write!(
+            f,
+            "{}\n{}\naqi: {}",
+            self.station.name.blue().bold(),
+            self.station.url,
+            aqi
+        )
+    }
+}
 #[derive(Debug, Deserialize)]
 struct STime {
     stime: String,
     tz: String,
     vtime: u32,
 }
+
+// fn format_aqi(num: u32) -> FgDynColorDisplay {
+//     match num {
+//         0..=50 => {
+//             num.color(CssColors::Green)
+//         }
+//         51..=100 => {
+//             num.color(CssColors::Yellow)
+//         }
+//         101..=150 => {
+//             num.color(CssColors::Orange)
+//         }
+//         151..=200 => {
+//             num.color(CssColors::Red)
+//         }
+//         201..=300 => {
+//             num.color(CssColors::Purple)
+//         }
+//         301..=500 => {
+//             num.color(CssColors::Maroon)
+//         }
+//         _ => {
+//             num.color(CssColors::White)
+//         }
+// }
